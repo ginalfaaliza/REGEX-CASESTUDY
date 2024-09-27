3a510de319978ae9dab2bb0bdf52274ad4b3cddb
Ginalfa Aliza Labikha Sanaj <125278556+ginalfaaliza@users.noreply.github.com>
2024-09-27 10:56:45 +0700

    Update dqlabregex.sql

--- dqlabregex.sql
+++ dqlabregex.sql
@@ -1,8 +1,12 @@
 #Extract all data containing city information that ends with 'ng'
 SELECT * FROM dqlabregex WHERE kota REGEXP 'ng$';
 
-#Provide information on the record staff with the names "Senja" and "Sendja"
+#Provide information on the 'staf_pencatat' with the names "Senja" and "Sendja"
 SELECT * FROM dqlabregex WHERE staf_pencatat REGEXP 'sen.?ja';
 
 #Check for possible input errors in the "member count" column
 SELECT * FROM dqlabregex WHERE jumlah_member REGEXP '^[0-9]';
+
+# Provide information on the 'staf_pencatat' with the first name with "AN" by ignoring case sensitive
+SELECT * FROM dqlabregex WHERE REGEXP_LIKE(staf_pencatat, '^AN');
+
